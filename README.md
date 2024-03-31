0f67c9b923ac9d8f782495d805db0670b25ed59d
溪如风 <542672041@qq.com>
2024-03-31 18:24:41 +0800

    perf: 开发博客项目之接口

---
+++ README.md
@@ -0,0 +1,21 @@
+<!--
+ * @Author: 溪如风
+ * @LastEditTime: 2024-03-31 18:23:42
+-->
+## Node.js 开发Web Server博客
+学习node.js, 不使用框架，开发一个 Web Server博客。
+
+### 功能
+#### blog 路由
+- 获取博客列表
+- 获取博客详情
+- 新建一篇博客
+- 更新一篇博客
+- 删除一篇博客
+#### user 路由
+- 登录
+
+## 总结
+- nodejs 处理 http 请求的常用技能，postman 的使用 [√]
+- nodejs 开发博客项目的接口(未连接数据库，未使用登录)[√]
+- 理解为何要将 router 和 controller 分开 [√]
--- src/controller/user.js
+++ src/controller/user.js
@@ -1,14 +1,15 @@
 /*
  * @Author: 溪如风
- * @LastEditors: chg
- * @LastEditTime: 2024-03-31 17:43:53
+ * @LastEditTime: 2024-03-31 18:20:20
  */
-const login = (username, password) => {
+const loginCheck = (username, password) => {
     if (username === 'zhangsan' && password === '123') {
-        return Promise.resolve({
-            msg: '登录成功'
-        })
+        return true
     } else {
-        return Promise.reject(new Error('登录失败'))
+        return false
     }
+}
+
+module.exports = {
+    loginCheck
 }
